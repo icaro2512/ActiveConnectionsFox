5142a91b565eec3ec5f3e5880367adbb9b245cb4
Icaro <icaro2512@hotmail.com>
2016-02-23 12:39:07 -0300

    Foxkbt 23/02/2016 12:38

--- app/src/main/java/info/lifeti/activeconnections11/Controller/TelaCliente.java
+++ app/src/main/java/info/lifeti/activeconnections11/Controller/TelaCliente.java
@@ -1,7 +1,10 @@
 package info.lifeti.activeconnections11.Controller;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import info.lifeti.activeconnections11.R;
 
@@ -11,5 +14,17 @@ public class TelaCliente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cliente);
+    }
+
+    public void clientInfos(View v) {
+        final Context context = this;
+        Intent it = new Intent(context, TelaClienteInformacoes.class);
+        startActivity(it);
+    }
+
+    public void voltaClients(View v) {// tirei pq tem botão de voltar no android ja
+        final Context context = this;
+        Intent it = new Intent(context, TelaClientes.class);
+        startActivity(it);
     }
 }
--- app/src/main/java/info/lifeti/activeconnections11/Controller/TelaClientes.java
+++ app/src/main/java/info/lifeti/activeconnections11/Controller/TelaClientes.java
@@ -1,5 +1,7 @@
 package info.lifeti.activeconnections11.Controller;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,12 @@ public class TelaClientes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_clientes);
+    }
+
+    public void clientOptions(View v) {
+        final Context context = this;
+        Intent it = new Intent(context, TelaCliente.class);
+        startActivity(it);
     }
 
     public void fechar(View v) {
