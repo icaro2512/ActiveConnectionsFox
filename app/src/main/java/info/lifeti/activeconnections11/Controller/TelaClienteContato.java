eab1dc77f756b4fa0804d158fe5a79d578259188
Icaro <icaro2512@hotmail.com>
2016-02-23 17:08:30 -0300

    Foxkbt                23/02/2016                17:08                client concluido

--- app/src/main/java/info/lifeti/activeconnections11/Controller/TelaClienteContato.java
+++ app/src/main/java/info/lifeti/activeconnections11/Controller/TelaClienteContato.java
@@ -1,5 +1,7 @@
 package info.lifeti.activeconnections11.Controller;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,12 @@ public class TelaClienteContato extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cliente_contato);
+    }
+
+    public void contatoEdit(View v) {
+        final Context context = this;
+        Intent it = new Intent(context, TelaClienteContatoTelEmail.class);
+        startActivity(it);
     }
 
     public void closeClientCont(View v) {
